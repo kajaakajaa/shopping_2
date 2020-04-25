055ec57132d583782e7b24931772003ddcb4e264
kajaakajaa <rwqxh8703@yahoo.co.jp>
2020-04-25 18:13:25 +0900

    add(settled(group_by)_15mac_4-25

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,3 @@
 class Item < ApplicationRecord
-  validates :name, :value, :number, presence:{ message: "空欄では、ん無理！"}
-  # validates :body, presence:{ message: "短いんじゃボケィ！！"}
+  validates :name, :value, :number, presence:{ message: "空欄では送信できません。"}
 end
