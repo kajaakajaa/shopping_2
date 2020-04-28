14774e6e05e1eb0ac06b4c449ae06ad107429c2c
kajaakajaa <rwqxh8703@yahoo.co.jp>
2020-04-28 20:07:26 +0900

    solved(js)_15mac_4-28

---
+++ app/javascript/packs/shopping/index.js
@@ -0,0 +1,17 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log(document.getElementById('hello'));
+// });
+
+"use strict;"
+
+for (let i = 0; i < 10; i++){
+  const div = document.createElement("div");
+  div.classList.add("box");
+  div.textContent = i;
+
+  div.addEventListener("click", () => {
+    div.classList.toggle("circle");
+  });
+
+  document.body.appendChild(div);
+}
