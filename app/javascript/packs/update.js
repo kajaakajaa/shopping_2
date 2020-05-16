f0e1fee7aa2a11c1cef4edad5353e0288fb3bccd
kajaakajaa <rwqxh8703@yahoo.co.jp>
2020-05-16 11:09:50 +0900

    add_15mac_5-16

--- app/javascript/packs/update.js
+++ app/javascript/packs/update.js
@@ -2,18 +2,17 @@
 //   console.log(document.getElementById('hello'));
 // });
 
-document.getElementById("kajaa").addEventListener("click", function(){ 
 var mydiv = document.getElementById("mydiv");
+document.getElementById("kajaa").addEventListener("click", function(){ 
 mydiv.innerHTML = "<h3>h1をh3へ変更しました！<h3>";
-});
-document.getElementById("kajaa").removeEventListener("click", function(){ 
-  document.getElementById("mydiv");
+}, false);
+
+var mydiv = document.getElementById("mydiv");
+mydiv.removeEventListener("click", function(){
+  mydiv.innerHTML = "変更前";
 });
 
-// document.getElementById("kajaa").addEventListener("click", function(){ 
-// var mydiv = document.getElementById("mydiv");
-// mydiv.innerHTML = "<h1>変更前<h1>";
-// });
+// 要素(オブジェクト).removeEventListener(イベントの種類, 関数[, オプション]);
 
 //以下 テスト。
 // "use strict;"
