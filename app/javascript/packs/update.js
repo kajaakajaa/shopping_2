8754203d3acad367fedc9388b52fd1a4640cb0b7
kajaa16 <rwqxh8703@yahoo.co.jp>
2020-05-16 23:18:35 +0900

    solved_16mac_5-16

--- app/javascript/packs/update.js
+++ app/javascript/packs/update.js
@@ -2,30 +2,26 @@
 //   console.log(document.getElementById('hello'));
 // });
 
-var mydiv = document.getElementById("mydiv");
-document.getElementById("kajaa").addEventListener("click", function(){ 
-mydiv.innerHTML = "<h3>h1をh3へ変更しました！<h3>";
-}, false);
+function yassa(e){
+  const str = document.getElementById("myh1").textContent;
 
-var mydiv = document.getElementById("mydiv");
-mydiv.removeEventListener("click", function(){
-  mydiv.innerHTML = "変更前";
-});
+  if(str=="変更前"){
+    var myh1 = document.getElementById("myh1");
+    myh1.innerHTML = "<h3>変更されました!<h3>";
+  }else if(str=="変更されました!"){
+    var myh1 = document.getElementById("myh1");
+    myh1.innerHTML = "変更前";
+  }
+}
+const kajaa = document.getElementById("kajaa");
+kajaa.addEventListener("click", yassa);
 
-// 要素(オブジェクト).removeEventListener(イベントの種類, 関数[, オプション]);
+// document.getElementById("kajaa").addEventListener("click", function(){ 
+// var mydiv = document.getElementById("mydiv");
+// mydiv.innerHTML = "<h3>h1をh3へ変更しました！<h3>";
+// }, false);
 
-//以下 テスト。
-// "use strict;"
+// document.getElementById("kajaa").addEventListener("click", function(){ 
+//   document.getElementById("kajaa").classList("")
+// }, false);
 
-// for (let i = 0; i < 10; i++){
-//   const div = document.createElement("div");
-//   div.classList.add("box");
-//   div.textContent = i;
-
-//   div.addEventListener("click", () => {
-//     div.classList.toggle("circle");
-//   });
-
-//   document.body.appendChild(div);
-// }
-//以上 テスト。
