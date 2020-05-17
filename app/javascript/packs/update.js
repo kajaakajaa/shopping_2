35417997509227b7c6770b4b5e7ee85e8f00d791
kajaakajaa <rwqxh8703@yahoo.co.jp>
2020-05-17 18:08:49 +0900

    (practice_file_create)_15mac_5-17

--- app/javascript/packs/update.js
+++ app/javascript/packs/update.js
@@ -1,27 +1,8 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   console.log(document.getElementById('hello'));
-// });
+document.getElementById("item_number_2").addEventListener("change", function(e){
+  var total = document.getElementById("total_td");
+  var number = document.getElementById("item_number_2");
+  var unit_price = document.getElementById("unit_price");
+  var oneitem_price = unit_price * number;
 
-function yassa(e){
-  const str = document.getElementById("myh1").textContent;
-
-  if(str=="変更前"){
-    var myh1 = document.getElementById("myh1");
-    myh1.innerHTML = "<h3>変更されました!<h3>";
-  }else if(str=="変更されました!"){
-    var myh1 = document.getElementById("myh1");
-    myh1.innerHTML = "変更前";
-  }
-}
-const kajaa = document.getElementById("kajaa");
-kajaa.addEventListener("click", yassa);
-
-// document.getElementById("kajaa").addEventListener("click", function(){ 
-// var mydiv = document.getElementById("mydiv");
-// mydiv.innerHTML = "<h3>h1をh3へ変更しました！<h3>";
-// }, false);
-
-// document.getElementById("kajaa").addEventListener("click", function(){ 
-//   document.getElementById("kajaa").classList("")
-// }, false);
-
+  total += total_td;
+});
