09ac7d243dc0b85ea8d506ab253c9ac1e3086bcc
kajaa16 <rwqxh8703@yahoo.co.jp>
2020-06-27 22:51:37 +0900

    16m_6-27

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,6 @@ class ItemsController < ApplicationController
     @items = Item.find_by_sql(["SELECT * FROM items WHERE id 
       IN(SELECT MAX(id) FROM items GROUP BY name);"]) # "name" カラムに group メソッド処理で絞り込み、
         # かつidの最大値(idの大きい数字順)を取得する。ActiveRecord に変換したメソッドは不明。
-    # @items.each do |item|
-    #   @valnums = item.value * item.number
-    #   @total = item.group(:name).sum(@valnums)
-    # end
-    # @total = Item.all.sum(:value)
     @total = 0 #(← 初期の数値設定が要る)
     @items.each do |item|
       @total += item.value * item.number
@@ -22,12 +17,12 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def update
-  #   @item = Item.find(params[:id])
-  #     if @item.update(update_params)
-  #       redeirect_to action: :index
-  #     end
-  # end
+  def update
+    @item = Item.find(params[:id])
+      if @item.update(update_params)
+        redeirect_to action: :index
+      end
+  end
 
   def destroy
   end
