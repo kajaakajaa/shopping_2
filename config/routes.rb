ffec4aef0fa8bb8140ef7dd9bb5830063b05c236
kajaa16 <rwqxh8703@yahoo.co.jp>
2020-07-16 15:39:16 +0900

    latest

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/items/" => "items#index"
   post "/items/" => "items#create"
   delete "/items/:id" => "items#destroy"
-  patch "/items/:id" => "items#update"
+  patch "/items" => "items#update"
   
   get "practice" => "items#practice"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
