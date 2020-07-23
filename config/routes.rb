d03971a262c42705fb0e14dced58b9a9b3edd00b
kajaa16 <rwqxh8703@yahoo.co.jp>
2020-07-23 18:45:32 +0900

    created_another_delete_16m_7-23

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "items#index"
   get "/items/" => "items#index"
   post "/items/" => "items#create"
-  delete "/items/:id" => "items#destroy", as: "delete"
+  delete "/items/update/:id" => "items#destroy", as: "delete"
   patch "/items/" => "items#update"
   
   get "practice" => "items#practice"
