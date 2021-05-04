1a03596ac0d66ddcd43d687e89a77911e7482b98
kajaakajaa <rwqxh8703@yahoo.co.jp>
2021-05-04 17:16:34 +0900

    production.rb url追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
 
    #mailer setting
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "https://kajaashopping.herokuapp.com/users/items" }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
