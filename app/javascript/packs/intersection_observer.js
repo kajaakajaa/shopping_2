371ce14e49fe06df322e64d3123729ae44438af4
kajaakajaa <rwqxh8703@yahoo.co.jp>
2021-05-17 22:02:49 +0900

    更新スクロールイベントの追加実装

---
+++ app/javascript/packs/intersection_observer.js
@@ -0,0 +1,30 @@
+(() => {
+    // レスポンシブ(モバイルviewのみ適用)。↓
+    const windowWidth = $(window).width();
+    const windowMbi = 411;
+    if (windowWidth <= windowMbi) {
+
+        // 交差監視対象要素の取得。
+        const total_tbl = document.querySelector(".container__total_wrap__tbl");
+        const updateBtn = document.querySelector(".container__total_wrap__updatesend");
+
+        const options = {
+            root: null,
+            rootMargin: "-80px"
+        };
+
+        const observer = new IntersectionObserver(callback, options);
+        observer.observe(total_tbl);
+
+        function callback(entries, obsever) {
+            entries.forEach(entry => {
+                if (!entry.isIntersecting) {
+                    updateBtn.classList.add("mvUpdateBtn");
+                }else{
+                    updateBtn.classList.remove("mvUpdateBtn");
+                }
+            });
+        }
+    }
+
+})();
