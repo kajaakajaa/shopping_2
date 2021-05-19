863ebf7022be0e9809483f669c31d964732cfad5
kajaakajaa <rwqxh8703@yahoo.co.jp>
2021-05-19 19:34:46 +0900

    2021/5-19_15mac(2)

--- app/javascript/packs/intersection_observer.js
+++ app/javascript/packs/intersection_observer.js
@@ -6,10 +6,8 @@
 
         // 交差監視対象要素の取得。
         const total_tbl = document.querySelector(".container__total_wrap__tbl");
-        // 更新ボタン移動先/移動前
-        const total_wrap = document.querySelector(".container__total_wrap");
-        const aft_mvUp = document.querySelector(".aft_mvUp");
-        const bill_div = document.querySelector(".container__index__bill_div");
+        // 更新ボタンの移動(表示/非表示)
+        const mvUpd_btn = document.querySelector(".container__index__bill_div__aft_mvUpd__btn");
         const updateBtn = document.querySelector(".container__total_wrap__updatesend");
 
         const options = {
@@ -22,23 +20,12 @@
 
         function callback(entries, obsever) {
             entries.forEach(entry => {
-                if (!entry.isIntersecting) {
-                    // console.log(!entry.isIntersecting);
-                    // updateBtn.classList.add("mvUpdateBtn");
-                    $(updateBtn).prependTo(aft_mvUp);
-                    console.log(total_wrap.classList);
-                    updateBtn.classList.toggle(".aft_mvUp__btn")
-                    // aft_mvUp.insertAdjacentHTML("afterbegin", `
-                    //     <input type="submit" class="aft_mvUp__btn"
-                    //         id="updatesend" name="commit" value="更新" form="update">
-                    // `);
-                // }else{
-                //     aft_mvUp.classList.remove("aft_mvUp__btn");
-                //     console.log(aft_mvUp.classList);
-                //     total_wrap.insertAdjacentHTML("beforeend", `
-                //         <input type="submit" class="container__total_wrap__updatesend"
-                //             id="updatesend" name="commit" value="更新" form="update">
-                //     `);
+                if (entry.isIntersecting) {
+                    updateBtn.style.visibility = "visible";
+                    mvUpd_btn.style.visibility = "hidden";
+                }else{
+                    updateBtn.style.visibility = "hidden";
+                    mvUpd_btn.style.visibility = "visible";
                 }
             });
         }
