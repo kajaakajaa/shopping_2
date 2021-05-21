48bf5a9186c850c11b8a0c33c885fe6696d212cb
kajaakajaa <rwqxh8703@yahoo.co.jp>
2021-05-21 14:53:57 +0900

    Rspec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :items, dependent: :destroy
-  validates_associated :items
+  # validates_associated :items
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :timeoutable
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # it "is invalid without a name" do
+
+  # end
+
+  # it 'is invalid without a name' do
+  #     ho = Hoge.new()
+  #     ho.valid?
+  #     expect(ho.errors.messages[:name]).to include('Can not be blank')
+  # end
+end
