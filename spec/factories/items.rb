e5fef24610856743b18e182bea35360390e50792
kajaakajaa <rwqxh8703@yahoo.co.jp>
2021-05-23 19:44:41 +0900

    2021/5/23_15mac(3)

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :item do
     sequence(:name) { |n| "kajaa#{n}" }
-    # sequence(:value) {  }
     association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  describe 'validation' do
-    
-    describe "association" do
+  describe "validation" do
+
+    describe "assciation" do
       t = described_class.reflect_on_association(:user)
       it { expect(t.macro).to eq(:belongs_to) }
     end
-    
+
     describe "name" do
-      context 'when "name" is blank' do
-        let (:item) { build(:item) }
+      context "when 'name' is blank " do
+        let(:item) { build(:item) }
         it { expect(item).to be_valid }
       end
     end
