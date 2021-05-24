a14cb5822a091263b89dc0b6c9d67fdddcc778c6
kajaakajaa <rwqxh8703@yahoo.co.jp>
2021-05-25 08:20:58 +0900

    2021/5.25_15mac

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :item do
     sequence(:name) { |n| "kajaa#{n}" }
+
     association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  describe "validation" do
-
+  describe "name_validation" do
+    let(:item) { build(:item) }
+    
     describe "assciation" do
-      t = described_class.reflect_on_association(:user)
-      it { expect(t.macro).to eq(:belongs_to) }
+      # t = described_class.reflect_on_association(:user)
+      # it { expect(t.macro).to eq(:belongs_to) }
+      it { should belong_to(:user) }
     end
-
+    
     describe "name" do
-      context "when 'name' is blank " do
-        let(:item) { build(:item) }
+      context "when 'name' have space " do
         it { expect(item).to be_valid }
+      end
+      context "when 'name' is uniqueness in 'user_id'" do
+        # it "does something with multiple users" do
+          # user = FactoryBot.create(:item)
+        # end
+        it { is_expected.not_to validate_uniqueness_of(item.name).scoped_to(:user_id) }
       end
     end
 
